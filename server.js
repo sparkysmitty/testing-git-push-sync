f9b7e48c13e2596e9f8e9a64dfd479e08658fbf4
Glitch (hello-express) <none>
2022-04-12 14:17:42 +0000

    🚌🎠 Checkpoint ./server.js:43651580/514

--- server.js
+++ server.js
@@ -3,13 +3,14 @@
 
 // we've started you off with Express (https://expressjs.com/)
 // but feel free to use whatever libraries or frameworks you'd like through `package.json`.
-const express = require("express");
-const app = express();
 
+const express = require("express");
 const bodyParser = require('body-parser');
-app.use(bodyParser.json());
 const crypto = require('crypto');
 const { execSync } = require('child_process');
+
+const app = express();
+app.use(bodyParser.json());
 
 // our default array of dreams
 const dreams = [
@@ -19,44 +20,47 @@ const dreams = [
 ];
 
 // make all the files in 'public' available
-// https://expressjs.com/en/starter/static-files.html
 app.use(express.static("public"));
 
-// https://expressjs.com/en/starter/basic-routing.html
 app.get("/", (request, response) => {
   response.sendFile(__dirname + "/views/index.html");
 });
 
 // send the default array of dreams to the webpage
 app.get("/dreams", (request, response) => {
-  // express helps us take JS objects and send them as JSON
   response.json(dreams);
 });
 
 app.post('/git', (req, res) => {
-  const hmac = crypto.createHmac('sha1', process.env.SECRET);
-  const sig  = 'sha1=' + hmac.update(JSON.stringify(req.body)).digest('hex');
-  if (req.headers['x-github-event'] === 'push' &&
-    crypto.timingSafeEqual(Buffer.from(sig), Buffer.from(req.headers['x-hub-signature']))) {
-    res.sendStatus(200);
-    const commands = ['git fetch origin master',
-                      'git reset --hard origin/master',
-                      'git pull origin master --force',
-                      'npm install',
-                      // your build commands here
-                      'refresh']; // fixes glitch ui
-    for (const cmd of commands) {
-      console.log(execSync(cmd).toString());
-    }
-    console.log('updated with origin/master!');
-    return;
-  } else {
-    console.log('webhook signature incorrect!');
-    return res.sendStatus(403);
-  }
+  
+  console.log("saw a post!")
+  
+  //const hmac = crypto.createHmac('sha1', process.env.SECRET);
+  //const sig  = 'sha1=' + hmac.update(JSON.stringify(req.body)).digest('hex');
+  //if (req.headers['x-github-event'] === 'push' &&
+  //  crypto.timingSafeEqual(Buffer.from(sig), Buffer.from(req.headers['x-hub-signature']))) {
+  //  res.sendStatus(200);
+  //  const commands = ['git fetch origin master',
+  //                    'git reset --hard origin/master',
+  //                    'git pull origin master --force',
+  //                    'npm install',
+  //                    // your build commands here
+  //                    'refresh']; // fixes glitch ui
+  //  for (const cmd of commands) {
+  //    console.log(execSync(cmd).toString());
+  //  }
+  //  console.log('updated with origin/master!');
+  //  return;
+  //} else {
+  //  console.log('webhook signature incorrect!');
+  //  return res.sendStatus(403);
+  //}
+  
 });
+
 
 // listen for requests :)
 const listener = app.listen(process.env.PORT, () => {
   console.log("Your app is listening on port " + listener.address().port);
 });
+
