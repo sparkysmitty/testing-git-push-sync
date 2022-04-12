913f4002caaae052896814b280b68a7927fd9f91
Glitch (hello-express) <none>
2022-04-12 14:40:43 +0000

    🕋🚜 Checkpoint ./server.js:43651580/175 ./views/index.html:43651580/588

--- server.js
+++ server.js
@@ -1,9 +1,8 @@
 const port = 8000;
+let express = require('express'), app = express();
+let bodyParser = require('body-parser');
 
-var express = require('express'), app = express();
-var bodyParser = require('body-parser');
-
-app.use(express.static(__dirname + '/public'));
+// app.use(express.static(__dirname + '/public'));
 
 app.use(bodyParser.urlencoded({
    extended: false
@@ -11,16 +10,18 @@ app.use(bodyParser.urlencoded({
 
 app.use(bodyParser.json());
 
-app.get('/', function(req, res){
-  res.render('form');// if jade
+// app.get('/', function(req, res){
+  // res.render('form');// if jade
   // You should use one of line depending on type of frontend you are with
-  res.sendFile(__dirname + '/form.html'); //if html file is root directory
- res.sendFile("index.html"); //if html file is within public directory
-});
+  // res.sendFile(__dirname + '/form.html'); //if html file is root directory
+  // let htmlData = "<html><body>Hello Fred</body></html"
+  // res.sendFile("index.html"); //if html file is within public directory
+  // res.send(htmlData)
+// });
 
 app.post('/',function(req,res){
-   var username = req.body.username;
-   var htmlData = 'Hello:' + username; // + "<br><br>" + process.env.SECRET;
+   let username = req.body.username;
+   let htmlData = 'Hello:' + username + "<br><br>" + process.env.SECRET
    res.send(htmlData);
    console.log(htmlData);
 });
